c14aff242c480089365c76d04acafcd67b6aa82f
Shubham Karande <shubhamkarande13@gmail.com>
2020-08-25 21:24:38 +0530

    add hashing using hashlib

--- crypto_hash.py
+++ crypto_hash.py
@@ -1,14 +1,18 @@
 import hashlib
+import json
+
 
 def crypto_hash(data):
     """
     Return a SHA-256 hash of the given data.
     """
+    stringified_data =  json.dumps(data)
+    return hashlib.sha256(stringified_data.encode('utf-8')).hexdigest()
 
-    return hashlib.sha256(data)
 
 def main():
     print(f"crypto_hash('foo'):{crypto_hash('foo')}")
+
 
 if __name__ == "__main__":
     main()
