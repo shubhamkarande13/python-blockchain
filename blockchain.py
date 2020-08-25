2ddaa6533c1c34e7ddbd8b938207066461abd8e5
Shubham Karande <shubhamkarande13@gmail.com>
2020-08-25 20:58:16 +0530

    Update add_block and Static Methods

--- blockchain.py
+++ blockchain.py
@@ -1,18 +1,21 @@
-from block import Block 
+from block import Block
+
+
 class Blockchain:
     '''
     Blockchain: a public ledger of transactions.
     Implemented as a list of blocks - data sets of transactions
     '''
 
-    def __init__ (self):
-        self.chain = []
+    def __init__(self):
+        self.chain = [Block.genesis()]
 
-    def add_block(self,data):
-        self.chain.append(Block(data))
+    def add_block(self, data):
+        self.chain.append(Block.mine_block(self.chain[-1], data))
 
     def __repr__(self):
         return f'Blockchain: {self.chain}'
+
 
 def main():
     blockchain = Blockchain()
@@ -20,7 +23,7 @@ def main():
     blockchain.add_block('two')
 
     print(blockchain)
-    print(f'blockchain.py __name__:{__name__}')
+    print(f'blockchain.py ___name__: {__name__}')
 
-    if __name__ == '__main__':
-        main()
+if __name__ == '__main__':
+    main()
