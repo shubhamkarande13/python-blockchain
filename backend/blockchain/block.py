83878dc016e6978b51a7740e7e3bade58c9fa7a5
Shubham Karande <shubhamkarande13@gmail.com>
2020-08-26 19:44:47 +0530

    add tests for block.py & blockchain.py

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,6 +1,13 @@
 import time
 from backend.util.crypto_hash import crypto_hash
 
+GENESIS_DATA = {
+    'timestamp': 1,
+    'last_hash': 'genesis_last_hash',
+    'hash': 'genesis_hash',
+    'data': []
+}
+
 
 class Block:
     '''
