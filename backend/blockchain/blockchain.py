ac93b9e5999b6071e40e5bbbfe5e14cbfb8f2a27
Shubham Karande <shubhamkarande13@gmail.com>
2020-09-10 00:54:30 +0530

    write tests to validate chain

--- backend/blockchain/blockchain.py
+++ backend/blockchain/blockchain.py
@@ -16,6 +16,22 @@ class Blockchain:
     def __repr__(self):
         return f'Blockchain: {self.chain}'
 
+    @staticmethod
+    def is_valid_chain(chain):
+        """
+        Validate the incoming chain.
+        Enforce the following rules of the blockchain:
+            - the chain must start with a genesis block
+            - blocks must be formatted correctly
+        """
+        if chain[0] != Block.genesis():
+            raise Exception('The genesis block must be valid')
+
+        for i in range(1, len(chain)):
+            block = chain[i]
+            last_block = chain[i-1]
+            Block.is_valid_block(last_block, block)
+
 
 def main():
     blockchain = Blockchain()
@@ -24,6 +40,7 @@ def main():
 
     print(blockchain)
     print(f'blockchain.py ___name__: {__name__}')
+
 
 if __name__ == '__main__':
     main()
